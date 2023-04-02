cb82129f4701f8cbf2ab766011e3737a55c29e89
aleaxim <56270967+aleaxim@users.noreply.github.com>
2023-04-02 19:15:27 +0800

    add custom fonts

---
+++ src/styles/globalStyles.js
@@ -0,0 +1,30 @@
+import {Dimensions} from 'react-native';
+
+export const dimensions = {
+  fullHeight: Dimensions.get('window').height,
+  fullWidth: Dimensions.get('window').width,
+};
+
+export const colors = {
+  //   primary: '#2E32CD',
+  //   secondary: '#CD2E3A',
+  //   tertiary: '#F0CB8A',
+  primaryRed: '#CD2E3A',
+  secondaryRed: '#8F3035',
+  primaryBlue: '#2E32CD',
+  secondaryBLue: '#2D3292',
+  background: '#fff',
+  darkBg: '#181616',
+  muted: '#6C757D',
+  lightgray: '#EDEDED',
+  darkgray: '#555555',
+  white: '#fff',
+};
+
+export const fonts = {
+  primary: '',
+  lato: 'Lato-Regular',
+  latoBold: 'Lato-Bold',
+  latoBlack: 'Lato-Black',
+  latoLight: 'Lato-Light',
+};
