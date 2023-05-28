29a7ed44b8c5c0aca3c4bdb6a1876f25ec7923e3
aleaxim <patriciamichaela@outlook.com>
2023-05-29 00:47:38 +0800

    build

--- src/api.js
+++ src/api.js
@@ -1,2 +1,7 @@
-export const API_URL = 'http://10.0.2.2:8000/api';
-export const APP_URL = 'http://10.0.2.2:8000';
+// LOCAL
+// export const API_URL = 'http://10.0.2.2:8000/api';
+// export const APP_URL = 'http://10.0.2.2:8000';
+
+// LIVE
+export const API_URL = 'https://met4-digital.tech/api';
+export const APP_URL = 'https://met4-digital.tech';
